19499c2e5d4f85e12e84b576d3d1699e6fab5dc8
Christopher F <foxbot@protonmail.com>
2017-05-26 21:55:42 -0400

    Initial c# bot

---
+++ src/DiscordBot/Modules/InfoModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public Task Info()
+            => ReplyAsync(
+                $"Hello, I am a bot called {Context.Client.CurrentUser.Username} written in Discord.Net 1.0\n");
+    }
+}
---
+++ src/DiscordBot/Services/CommandHandlingService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot.Services
+{
+    public class CommandHandlingService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private IServiceProvider _provider;
+
+        public CommandHandlingService(IServiceProvider provider, DiscordSocketClient discord, CommandService commands)
+        {
+            _discord = discord;
+            _commands = commands;
+
+            _discord.MessageReceived += MessageReceived;
+        }
+
+        public async Task InitializeAsync(IServiceProvider provider)
+        {
+            _provider = provider;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            // Add additional initialization code here...
+        }
+
+        private async Task MessageReceived(SocketMessage rawMessage)
+        {
+            // Ignore system messages and messages from bots
+            if (!(rawMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            int argPos = 0;
+            if (!message.HasMentionPrefix(_discord.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(_discord, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _provider);
+
+            if ((result is ParseResult || 
+                result is PreconditionResult || 
+                result is ExecuteResult || 
+                result is TypeReaderResult) &&
+                !result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ToString());
+        }
+    }
+}
