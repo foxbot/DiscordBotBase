99b1cf5ef431ded8fa247d23dc543c1ffe8b22e0
AntiTcb <tcbskater@hotmail.com>
2017-06-02 21:32:56 -0400

    Initial VB bot

---
+++ src/DiscordBot/Modules/InfoModule.vb
@@ -0,0 +1,13 @@
+﻿Imports Discord.Commands
+
+Namespace DiscordBot.Modules
+    Public Class InfoModule
+        Inherits ModuleBase(Of SocketCommandContext)
+
+        <Command("Info")>
+        Public Function Info() As Task
+            Return ReplyAsync($"Hello, I am a bot called {Context.Client.CurrentUser.Username} written in Discord.Net 1.0")
+        End Function
+    End Class
+End Namespace
+
---
+++ src/DiscordBot/Program.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports Discord
+Imports Discord.Commands
+Imports Discord.WebSocket
+Imports DiscordBot.DiscordBot.Services
+Imports Microsoft.Extensions.Configuration
+Imports Microsoft.Extensions.DependencyInjection
+
+Module Program
+
+    Dim _client As DiscordSocketClient
+    Dim _config As IConfiguration
+
+    Sub Main()
+        MainAsync().GetAwaiter().GetResult()
+    End Sub
+
+    Async Function MainAsync() As Task
+        _client = New DiscordSocketClient
+        _config = BuildConfig()
+
+        Dim services = ConfigureServices()
+
+        services.GetRequiredService(Of LogService)
+        Await services.GetRequiredService(Of CommandHandlingService).InitializeAsync(services)
+
+        Await _client.LoginAsync(TokenType.Bot, _config("token"))
+        Await _client.StartAsync()
+
+        Await Task.Delay(-1)
+    End Function
+
+    Function ConfigureServices() As IServiceProvider
+        Dim collection As New ServiceCollection
+
+        With collection
+            .AddSingleton(_client)
+            .AddSingleton(Of CommandService)
+            .AddSingleton(Of CommandHandlingService)
+            .AddLogging()
+            .AddSingleton(Of LogService)
+            .AddSingleton(_config)
+        End With
+
+        Return collection.BuildServiceProvider()
+    End Function
+
+    Function BuildConfig() As IConfiguration
+        Return New ConfigurationBuilder() _
+            .SetBasePath(Directory.GetCurrentDirectory) _
+            .AddJsonFile("config.json") _
+            .Build()
+    End Function
+End Module
